98db4ede11ffdd31c68ad90be5eeafa0684ca851
nvmbr-hub <vlad.sn.s0n@gmai.com>
2021-11-22 18:06:52 +0300

    Create main page #1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const Header = () => {
+  return (
+    <div className="header">
+      <a>HOME</a>
+      <a>FEED</a>
+      <a>CONTACT</a>
+    </div>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div>
+        <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
